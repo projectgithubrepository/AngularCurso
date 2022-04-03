e10e05b3bf7f1fb2a8941fa12365d4b9055da07c
Daniel Rotband <rotbandnunes@hotmail.com>
2022-04-03 02:12:45 -0300

    binding attribute, class e style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,63 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// binding attribute, class e style
+
+//attribute binding
+// colspan é um atributo do table row, enquanto que colSpan é uma propriedade
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     <!-- <p [attr.attribute-you-are-targeting]="expression:any"></p> -->
+//     <tr>
+//       <td [attr.colspan]="1+1">one-two</td>
+//     </tr>
+//   `,
+//   styleUrls: ['./app.component.css']
+// })
+
+//class binding
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     <!-- uma classe -->
+//     <div [class.sale]="onSale"></div>  
+//     <!-- várias classes em uma string 
+//     <div [class]="'classOne classTwo'"></div> -->
+//     <!-- várias classes em um objeto 
+//     <div [class]="{
+//       classOne: true, 
+//       classTwo: true 
+//     }"></div> -->
+//     <!-- várias classes em um array
+//     <div [class]="['classOne classTwo']"></div> -->
+
+//   `,
+//   styleUrls: ['./app.component.css']
+// })
+
+
+//style binding
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     <!-- uma classe (pode ser tanto dashcase quanto camelcase) -->
+//     <div [style.background-color]="expression"></div>
+//     <div [style.backgroundColor]="expression"></div> 
+//     <!-- várias classes em uma string 
+//     <div [style]="'width: 200px; height: 200px; background: red;'"></div> -->
+//     <!-- várias classes em um objeto 
+//     <div [style]="{
+//       width: '200px', 
+//       height: '200px',
+//       background: 'green',
+//     }"></div> -->
+//     <!-- array no estilo não é suportado -->
+//   `,
+//   styleUrls: ['./app.component.css']
+// })
+
 export class AppComponent {
   //first lesson
   // title = 'angular-video-series';
@@ -42,6 +99,15 @@ export class AppComponent {
   // }
 
   //Pipes (datepipe, uppercasepipe, currencypipe, percentpipe etc)
-  todaysDate = new Date;
-  
+  // todaysDate = new Date;
+
+  // Property binding
+  //   itemImageUrl = '../assets/phone.jpg';
+
+  //   isUnchanged = true;
+
+  //class binding
+  //uma classe
+  // onSale = false;
+
 }
